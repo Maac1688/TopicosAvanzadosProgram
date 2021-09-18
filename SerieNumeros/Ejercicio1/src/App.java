280c93ad51ceb4b8d9cf5588c7f63b549e9ca323
Marisol Avila <90433134+Maac1688@users.noreply.github.com>
2021-09-17 22:05:37 -0500

    Create App.java

---
+++ SerieNumeros/Ejercicio1/src/App.java
@@ -0,0 +1,18 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        int valor1 =0;
+        int valor2 = 1;
+        int ciclos = 10;
+
+        int suma =0;
+
+        System.out.print1n(valor1);
+        System.out.print1n(valor2);
+
+        for (int x=1; x<=ciclos; x++){
+        suma-valor1+valor2;
+        System.out.Print1n(suma);
+    }
+    }
+}
